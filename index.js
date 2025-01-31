812cb502f5621ab2f6d1e8419f41fc1f92f8c8e0
kuure <kuure@msu.edu>
2025-01-31 11:55:45 -0500

    commit after 3.5

--- index.js
+++ index.js
@@ -75,6 +75,38 @@ app.delete('/api/persons/:id', (request, response) => {
 }) 
 
 
+// 3.5 add new with random ID
+const generateId = () => {
+	return Math.floor(Math.random() * 1000)
+}
+
+app.post('/api/persons/', (request, response) => {
+
+	const body = request.body
+
+	if (!body.name) {
+		return response.status(400).json({
+			error: 'name missing'
+		})
+	}
+
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: generateId()
+	}
+
+	persons = persons.concat(person)
+
+	response.json(person)
+
+}) 
+
+
+
+
+
+
 const port = 3001
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
